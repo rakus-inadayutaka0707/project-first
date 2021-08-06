1faa842d49137e7194d56734025e22bb46ae0437
rakus-inadayutaka0707 <yutaka.inada@rakus-partners.co.jp>
2021-08-07 08:25:09 +0900

    first commit

---
+++ src/main/java/com/example/ExTestApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExTestApplication.class, args);
+	}
+
+}
